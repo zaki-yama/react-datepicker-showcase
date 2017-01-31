d86afa32172bf015c69cf71263fa0e4fe0af5114
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2017-01-31 23:42:08 +0900

    add React-LDS:DateInput

---
+++ components/react-lightning-design-system/MyDateInput.js
@@ -0,0 +1,36 @@
+import React, { PropTypes } from 'react';
+import { DateInput } from 'react-lightning-design-system';
+
+export default class MyDateInput extends React.Component {
+  static get propTypes() {
+    return {
+      value: PropTypes.string,
+      onChange: PropTypes.func,
+    };
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: React.PropTypes.string,
+    };
+  }
+
+  onChange(value) {
+    this.setState({ value });
+    if (this.props.onChange) {
+      this.props.onChange(value);
+    }
+  }
+
+  render() {
+    return (
+      <DateInput
+        label='DateInput'
+        value={this.state.value}
+        onValueChange={(value) => this.onChange(value)}
+      />
+    );
+  }
+}
